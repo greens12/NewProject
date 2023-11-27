45d97f44e613fa1d39df999d2050dd93ca85b232
greens12 <razasara21292@gmail.com>
2023-11-27 23:36:56 +0530

    adding company details

---
+++ src/org/comp/CompanyInfo.java
@@ -0,0 +1,31 @@
+package org.comp;
+
+public class CompanyInfo {
+	
+	private void compName() {
+       System.out.println("Comp name:TCS");
+	}
+	
+	private void compId() {
+       System.out.println("Comp id:234567");
+	}
+	
+	private void compAdd() {
+      System.out.println("Comp add:chennai");
+	}
+
+	public static void main(String[] args) {
+		
+		CompanyInfo c = new CompanyInfo();
+		c.compName();
+		c.compId();
+		c.compAdd();
+		
+		
+		
+		
+		
+		
+		
+	}
+}
